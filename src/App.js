46be6846cd4c9410ae21ff307d7b5abe0d51bb3b
AdityaPare <adityapare2001@gmail.com>
2024-04-05 01:02:58 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'rsuite/dist/rsuite.min.css';
+import './styles/main.scss';
+function App() {
+  return (
+    <>
+      <h1>Hello </h1>
+    </>
+  );
+}
+
+export default App;
